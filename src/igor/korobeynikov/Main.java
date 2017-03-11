b79dd7b2a42377ab36a8edcb7b35e7933e1c090f
IGrock88 <igrock88@gmail.com>
2017-03-11 21:53:22 +0800

    Create main class. Create main Scene

---
+++ src/igor/korobeynikov/Main.java
@@ -0,0 +1,23 @@
+package igor.korobeynikov;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Chat client");
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
+    }
+}
